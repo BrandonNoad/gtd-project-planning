c9511b70e454d9a00b54ab775f8e97c349ea8775
Brandon Noad <Brandon.Noad@gmail.com>
2019-04-25 20:33:43 -0400

    Add netlify identity

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,30 @@
-import React from "react"
-import { Link } from "gatsby"
+import React, { useState, useEffect } from 'react';
+import netlifyIdentity from 'netlify-identity-widget';
 
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+import Layout from '../components/layout';
+import Image from '../components/image';
+import SEO from '../components/seo';
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
+const IndexPage = () => {
+    const [user, setUser] = useState();
 
-export default IndexPage
+    useEffect(() => {
+        netlifyIdentity.init();
+        netlifyIdentity.on('login', (user) => setUser(user));
+        netlifyIdentity.on('logout', () => setUser());
+    }, []);
+
+    return (
+        <Layout>
+            <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
+            <h1>Hello {user ? user.user_metadata.full_name : 'World!'}</h1>
+            <p>Welcome to your new Gatsby site.</p>
+            <p>Now go build something great.</p>
+            <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+                <Image />
+            </div>
+            <button onClick={() => netlifyIdentity.open()}>{user ? 'Log Out' : 'Log In'}</button>
+        </Layout>
+    );
+};
+export default IndexPage;
